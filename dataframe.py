79dc948a0dd610325556640bd791eaa7b09bb6a3
Allcharmian <91695211+Allcharmian@users.noreply.github.com>
2025-03-18 18:25:30 -0600

    cambio de JSON en un solo archivo

--- dataframe.py
+++ dataframe.py
@@ -14,6 +14,7 @@ import logging
 from pathlib import Path
 from typing import Dict, List, Any, Optional
 import pandas as pd
+import re
 
 # Configure logging
 logging.basicConfig(
@@ -23,11 +24,11 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Constants
-INPUT_FILE = 'JSON27K_output_cleaned.json'
+INPUT_FILE = 'JSON_27K.json'
 
 OUTPUT_FILE = 'output_table.csv'
-# INPUT_FILE = 'JSON14K.json'
-
+# INPUT_FILE = 'JSON14K_output_cleaned.json'
+OUTPUT_FILE2 = 'output.csv'
 # OUTPUT_FILE = 'output.csv'
 
 ACTION_TERMS = {
@@ -38,17 +39,32 @@ ACTION_TERMS = {
     "SHOW": "SHOW"
 }
 
+SET_PATTERN = r'(?:simpleVariables|complexVariables)\.set\(\s*[\'"](.*?)[\'"]\s*,\s*(.*?)\s*\)'
+
+MODIFY_PATTERN = r'(?:simpleVariables|complexVariables)\.modify\(\s*[\'"](.*?)[\'"]\s*,\s*(.*?)\s*\)'
+
+
+def remove_specific_fields(data, field_name):
+    if isinstance(data, dict):
+        return {k: remove_specific_fields(v, field_name) for k, v in data.items() if k != field_name}
+    elif isinstance(data, list):
+        return [remove_specific_fields(item, field_name) for item in data if item != field_name]
+    else:
+        return data
+
+
+# List of field names to be removed
+FIELDS_TO_REMOVE = [
+    'position', 'top', 'left', 'width', 'minWidth', 'height',
+    'zIndex', 'minHeight', 'responsiveHidden', 'subOrder',
+    'fieldId', 'FIELD', 'attemptsToGetContractGenerated', 'componentType', 'validatePreviousFieldsAndActions',
+    'blockFields', 'blockWhileProcessing', 'blockFields', 'specificationId',
+    'specification', 'businessLogicRelationship', 'componentKey', 'stringFunctionPreview'
+]
+
 
 def transform_json_to_table(data: Dict[str, Any]) -> Optional[pd.DataFrame]:
-    """
-    Transform JSON data into a structured DataFrame.
 
-    Args:
-        data: Dictionary containing businessLogicList and componentList
-
-    Returns:
-        DataFrame containing the transformed data or None if invalid input
-    """
     if not isinstance(data, dict):
         logger.error("Input data must be a dictionary")
         return None
@@ -71,10 +87,9 @@ def transform_json_to_table(data: Dict[str, Any]) -> Optional[pd.DataFrame]:
                 "ACCION": ACTION_TERMS.get(bl.get('event', ''), 'No disponible'),
                 "BL": bl.get("key", "No disponible"),
                 "NODO": bl.get('key', 'No disponible'),
-                "INPUT": "No disponible",
-                "OUTPUT": "No disponible",
-                "TIPO VAR": "No disponible",
-                "TIPO DATO": "No disponible",
+                "VARIABLE": "No disponible",
+                "PARAMETROS": "No disponible",
+                "SET o MODIFY": "No disponible",
                 "CODIGO": bl.get('validatePreviousFieldsAndActions', 'No disponible')
             }
             rows.append(row)
@@ -82,37 +97,35 @@ def transform_json_to_table(data: Dict[str, Any]) -> Optional[pd.DataFrame]:
         # Extract componentList data
         for comp in data['componentList']:
             tipo_var_list = []
-            tipo_dato_list = []
-            tipo_output = []
-            input_var_list = []
-            input_dato_list = []
-            if_var_list = []
-            if_dato_list = []
+            parametros_list = []
+            opcion_de_list = []
+            set_matches = []
+            modify_matches = []
+
             meta = comp.get('meta', {})
 
-            # Process Simple Variables output
-            for simple_var in meta.get('codeFactoryActionSimpleVariables', []):
-                for var in simple_var.get('variables', []):
-                    tipo_var_list.append(var.get('propertyName', 'No disponible'))
-                    tipo_dato_list.append(var.get('dataType', 'No disponible'))
-                    tipo_output.append(var.get('jajajajxdxdxdxjajajajajxdx', 'simpleVar'))
+            if 'meta' in comp and 'stringFunction' in comp['meta']:
+                code_section = comp['meta']['stringFunction']
 
-            # Process Complex Variables output
-            for complex_var in meta.get('codeFactoryActionComplexVariables', []):
-                for var in complex_var.get('variables', []):
-                    tipo_var_list.append(var.get('propertyName', 'No disponible'))
-                    tipo_dato_list.append(var.get('dataType', 'No disponible'))
-                    tipo_output.append(var.get('jajajajajajaxdxdjajajajxdx', 'complexVar'))
+                set_matches.extend(re.findall(SET_PATTERN, code_section))
+
+                modify_matches.extend(re.findall(MODIFY_PATTERN, code_section))
+
+            for match in set_matches:
+                tipo_var_list.extend({match[0]})
+                parametros_list.extend({match[1]})
+                opcion_de_list.append('set')
+
+            for match in modify_matches:
+                tipo_var_list.extend({match[0]})
+                parametros_list.extend({match[1]})
+                opcion_de_list.append('modify')
 
             # Process Action Parameters input
 
-            for Action_var in meta.get('codeFactoryActionParameters', []):
-                input_var_list.append(Action_var.get('parameterAlias', 'NO disponible'))
-                input_dato_list.append(Action_var.get('dataType', 'No disponible'))
-
-
             # Create rows for each variable combination
-            if not (tipo_var_list or input_var_list):  # If no variables found, create one default row
+            # If no variables found, create one default row
+            if not tipo_var_list:
                 rows.append({
                     "FLUJO": comp.get('specification', {}).get('specificationId', 'No disponible'),
                     "PAGINA": "No disponible",
@@ -122,15 +135,13 @@ def transform_json_to_table(data: Dict[str, Any]) -> Optional[pd.DataFrame]:
                     "ACCION": meta.get('title', 'No disponible'),
                     "BL": comp.get("componentKeyGenerated", "No disponible"),
                     "NODO": comp.get("componentKeyGenerated", "No disponible"),
-                    "INPUT": "No disponible",
-                    "OUTPUT": 'No disponible',
-                    "TIPO VAR": "No disponible",
-                    "TIPO DATO": "No disponible",
-                    "CODIGO": meta.get('stringFunction', 'No disponible')
+                    "VARIABLE": "No disponible",
+                    "PARAMETROS": "No disponible",
+                    "SET o MODIFY": "No disponible",
+                    "CODIGO": "No disponible"
                 })
-            elif (input_var_list and tipo_var_list):
-
-                for var_input, dato_input in zip(input_var_list, input_dato_list):
+            else:
+                for tipo_var, tipo_dato, tipo_opcion in zip(tipo_var_list, parametros_list, opcion_de_list):
                     rows.append({
                         "FLUJO": comp.get('specification', {}).get('specificationId', 'No disponible'),
                         "PAGINA": "No disponible",
@@ -140,40 +151,19 @@ def transform_json_to_table(data: Dict[str, Any]) -> Optional[pd.DataFrame]:
                         "ACCION": "No disponible",
                         "BL": comp.get("componentKeyGenerated", "No disponible"),
                         "NODO": comp.get('componentKeyGenerated', 'No disponible'),
-                        "INPUT": var_input,
-                        "OUTPUT": '',
-                        "TIPO VAR": 'No disponible',
-                        "TIPO DATO": dato_input,
-                        "CODIGO": meta.get('stringFunction', 'No disponible')
+                        "VARIABLE": tipo_var,
+                        "PARAMETROS": tipo_dato,
+                        "SET o MODIFY": tipo_opcion,
+                        "CODIGO": "No disponible"
                     })
-                for tipo_var, tipo_dato, output_tipe in zip(tipo_var_list, tipo_dato_list, tipo_output):
-                    rows.append({
-                        "FLUJO": comp.get('specification', {}).get('specificationId', 'No disponible'),
-                        "PAGINA": "No disponible",
-                        "ESTATUS": "No disponible",
-                        "COMPONENTE": comp.get('component', 'No disponible'),
-                        "TITULO COMPONENTE": meta.get('title', 'No disponible'),
-                        "ACCION": "No disponible",
-                        "BL": comp.get("componentKeyGenerated", "No disponible"),
-                        "NODO": comp.get('componentKeyGenerated', 'No disponible'),
-                        "INPUT": '',
-                        "OUTPUT": tipo_var,
-                        "TIPO VAR": output_tipe,
-                        "TIPO DATO": tipo_dato,
-                        "CODIGO": meta.get('stringFunction', 'No disponible')
-                    })
-
-
 
         return pd.DataFrame(rows)
-
     except Exception as e:
         logger.error(f"Error processing data: {str(e)}")
         return None
 
-
 def main() -> None:
-    """Main execution function."""
+
     try:
         input_path = Path(INPUT_FILE)
         if not input_path.exists():
@@ -183,13 +173,22 @@ def main() -> None:
         with open(input_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
 
-        df_table = transform_json_to_table(data)
+        for field in FIELDS_TO_REMOVE:
+            cleaned_data = remove_specific_fields(data, field)
+            if cleaned_data is None:
+                raise ValueError("Failed to transform data")
+
+        df_table = transform_json_to_table(cleaned_data)
         if df_table is None:
             raise ValueError("Failed to transform data")
 
         output_path = Path(OUTPUT_FILE)
         df_table.to_csv(output_path, index=False)
         logger.info(f"Table exported to '{output_path}'")
+
+        output_path2 = Path(OUTPUT_FILE2)
+        df_table.to_csv(output_path2, mode='a', index=False)
+        logger.info(f"Table exported to '{output_path2}'")
 
     except json.JSONDecodeError:
         logger.error("Invalid JSON format in input file")
